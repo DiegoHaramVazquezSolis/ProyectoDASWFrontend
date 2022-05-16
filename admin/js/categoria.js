a5b43eff1fba23f1d3496bd628ce0b6ab15f4c08
Molisawa <ernestobvc1@gmail.com>
2022-05-16 09:40:03 -0500

    crud complete

--- admin/js/categoria.js
+++ admin/js/categoria.js
@@ -18,3 +18,25 @@ async function addCategoria() {
         alert('No se pudo agregar el categoria');
     }
 }
+
+async function editCategoria() {
+    const id = document.getElementById('id_categoria').value;
+    const name = document.getElementById('categoria_name').value;
+    const editResponse = await fetch(`${API_URL}/api/v1/categories/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': localStorage.getItem('userToken')
+        },
+        body: JSON.stringify({
+            name
+        })
+    });
+
+    if (editResponse.status === 200) {
+        alert('Categoria editado correctamente');
+        window.location.replace('./catalogo.html');
+    } else {
+        alert('No se pudo editar el categoria');
+    }
+}
